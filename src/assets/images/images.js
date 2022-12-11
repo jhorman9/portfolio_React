bc0f85fbff57a0718f79bb5636773c6cc37d4e96
jhorman9 <jhosmita9@gmail.com>
2022-12-11 15:26:11 -0500

    actualizar

---
+++ src/assets/images/images.js
@@ -0,0 +1,47 @@
+import htmlIcon from './html5.png'
+import cssIcon from './CSS3.png'
+import javascriptIcon from './Javascript.png'
+import reduxIcon from './redux.png'
+import boostrapIcon from './boostrap.png'
+import reactIcon from './react.png'
+import ecommercePicture from './projects/ecommercereact.png'
+import pokemonPicture from './projects/pokemonreact.png'
+import pokemonCardPicture from './projects/pokemoncard.png'
+import rickAndMortyPicture from './projects/rickandmorty.png'
+import climaPicture from './projects/climareact.png'
+import contriesPicture from './projects/contries.png'
+import ecommerceVanillaPicture from './projects/ecommercevanila.png'
+import generatorCssVanilaPicture from './projects/generatorcss.png'
+import pianovanilaPicture from './projects/pianovanila.png'
+import rickAndMortyVanilaPicture from './projects/rickandmortyvanila.png'
+import phrasePicture from './projects/phrasereact.png'
+import todoVanilaPicture from './projects/todovanila.png'
+import retoFrontendPicture from './projects/retofrontend.png'
+import retoFrontendPicture2 from './projects/retofrontend2.png'
+import retoFrontendPicture3 from './projects/retofrontend3.png'
+import retoFrontendPicture4 from './projects/retofrontend4.png'
+
+export default {
+    html: htmlIcon,
+    css: cssIcon,
+    javascript: javascriptIcon,
+    redux: reduxIcon,
+    boostrap: boostrapIcon,
+    react: reactIcon,
+    ecommerce: ecommercePicture,
+    pokemon: pokemonPicture,
+    pokemonCard: pokemonCardPicture,
+    rickAndMorty: rickAndMortyPicture,
+    clima: climaPicture,
+    contries: contriesPicture,
+    ecommerceVanila: ecommerceVanillaPicture,
+    generatorCssVanila: generatorCssVanilaPicture,
+    pianoVanila: pianovanilaPicture,
+    rickAndMortyVanila: rickAndMortyVanilaPicture,
+    phrase: phrasePicture,
+    toDoVanila: todoVanilaPicture,
+    retoFrontend: retoFrontendPicture,
+    retoFrontend2: retoFrontendPicture2,
+    retoFrontend3: retoFrontendPicture3,
+    retoFrontend4: retoFrontendPicture4
+}
--- src/assets/page/Projects.jsx
+++ src/assets/page/Projects.jsx
@@ -1,27 +1,6 @@
 import React from 'react';
 import '../styles/Projects.css'
-import htmlIcon from '../images/html5.png'
-import cssIcon from '../images/css3.png'
-import javascriptIcon from '../images/javascript.png'
-import reduxIcon from '../images/redux.png'
-import boostrapIcon from '../images/boostrap.png'
-import reactIcon from '../images/react.png'
-import ecommercePicture from '../images/projects/ecommercereact.png'
-import pokemonPicture from '../images/projects/pokemonreact.png'
-import pokemonCardPicture from '../images/projects/pokemoncard.png'
-import rickAndMortyPicture from '../images/projects/rickandmorty.png'
-import climaPicture from '../images/projects/climareact.png'
-import contriesPicture from '../images/projects/contries.png'
-import ecommerceVanillaPicture from '../images/projects/ecommercevanila.png'
-import generatorCssVanilaPicture from '../images/projects/generatorcss.png'
-import pianovanilaPicture from '../images/projects/pianovanila.png'
-import rickAndMortyVanilaPicture from '../images/projects/rickandmortyvanila.png'
-import phrasePicture from '../images/projects/phrasereact.png'
-import todoVanilaPicture from '../images/projects/todovanila.png'
-import retoFrontendPicture from '../images/projects/retofrontend.png'
-import retoFrontendPicture2 from '../images/projects/retofrontend2.png'
-import retoFrontendPicture3 from '../images/projects/retofrontend3.png'
-import retoFrontendPicture4 from '../images/projects/retofrontend4.png'
+import images from '../images/images';
 
 const Projects = () => {
     return (
@@ -30,171 +9,171 @@ const Projects = () => {
             <div className='Projects__card-container'>
                 <a href="https://ecommerce-jhorman-vite.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={ecommercePicture} alt="" />
+                        <img className='Projects__img' src={images.ecommerce} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={reactIcon} />
-                            <img className='Projects__icons' src={reduxIcon} alt="" />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
-                            <img className='Projects__icons' src={boostrapIcon} alt="" />
+                            <img className='Projects__icons' src={images.react} />
+                            <img className='Projects__icons' src={images.redux} alt="" />
+                            <img className='Projects__icons' src={images.css} alt="" />
+                            <img className='Projects__icons' src={images.boostrap} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://pokedex-jhorman.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={pokemonPicture} alt="" />
+                        <img className='Projects__img' src={images.pokemon} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={reactIcon} />
-                            <img className='Projects__icons' src={reduxIcon} alt="" />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
+                            <img className='Projects__icons' src={images.react} />
+                            <img className='Projects__icons' src={images.redux} alt="" />
+                            <img className='Projects__icons' src={images.css} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://rickandmortyreact-jhorman.netlify.app/" target='_blank'> 
                     <div className="Projects__card">
-                        <img className='Projects__img' src={rickAndMortyPicture} alt="" />
+                        <img className='Projects__img' src={images.rickAndMorty} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={reactIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
+                            <img className='Projects__icons' src={images.react} />
+                            <img className='Projects__icons' src={images.css} alt="" />
                         </div>
                     </div>                
                 </a>
                 <a href="https://clima-jhorman.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={climaPicture} alt="" />
+                        <img className='Projects__img' src={images.clima} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={reactIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
+                            <img className='Projects__icons' src={images.react} />
+                            <img className='Projects__icons' src={images.css} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://countries-jhorman.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={contriesPicture} alt="" />
+                        <img className='Projects__img' src={images.contries} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={reactIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
+                            <img className='Projects__icons' src={images.react} />
+                            <img className='Projects__icons' src={images.css} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://pokemonapis-jhorman.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={pokemonCardPicture} alt="" />
+                        <img className='Projects__img' src={images.pokemonCard} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={reactIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
+                            <img className='Projects__icons' src={images.react} />
+                            <img className='Projects__icons' src={images.css} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://phrase-jhorman.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={phrasePicture} alt="" />
+                        <img className='Projects__img' src={images.phrase} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={reactIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
+                            <img className='Projects__icons' src={images.react} />
+                            <img className='Projects__icons' src={images.css} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://ecommerce-jhormannieto.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={ecommerceVanillaPicture} alt="" />
+                        <img className='Projects__img' src={images.ecommerceVanila} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={htmlIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
-                            <img className='Projects__icons' src={javascriptIcon} alt="" />
+                            <img className='Projects__icons' src={images.html} />
+                            <img className='Projects__icons' src={images.css} alt="" />
+                            <img className='Projects__icons' src={images.javascript} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://generatorcss-jhorman.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={generatorCssVanilaPicture} alt="" />
+                        <img className='Projects__img' src={images.generatorCssVanila} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={htmlIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
-                            <img className='Projects__icons' src={javascriptIcon} alt="" />
+                            <img className='Projects__icons' src={images.html} />
+                            <img className='Projects__icons' src={images.css} alt="" />
+                            <img className='Projects__icons' src={images.javascript} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://to-do-jhorman.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={todoVanilaPicture} alt="" />
+                        <img className='Projects__img' src={images.toDoVanila} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={htmlIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
-                            <img className='Projects__icons' src={javascriptIcon} alt="" />
+                            <img className='Projects__icons' src={images.html} />
+                            <img className='Projects__icons' src={images.css} alt="" />
+                            <img className='Projects__icons' src={images.javascript} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://piano-jhorman.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={pianovanilaPicture} alt="" />
+                        <img className='Projects__img' src={images.pianoVanila} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={htmlIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
-                            <img className='Projects__icons' src={javascriptIcon} alt="" />
+                            <img className='Projects__icons' src={images.html} />
+                            <img className='Projects__icons' src={images.css} alt="" />
+                            <img className='Projects__icons' src={images.javascript} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://third-reto-jhorman.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={retoFrontendPicture} alt="" />
+                        <img className='Projects__img' src={images.retoFrontend} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={htmlIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
+                            <img className='Projects__icons' src={images.html} />
+                            <img className='Projects__icons' src={images.css} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://portfolio-jhorman.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={retoFrontendPicture2} alt="" />
+                        <img className='Projects__img' src={images.retoFrontend2} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={htmlIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
-                            <img className='Projects__icons' src={javascriptIcon} alt="" />
+                            <img className='Projects__icons' src={images.html} />
+                            <img className='Projects__icons' src={images.css} alt="" />
+                            <img className='Projects__icons' src={images.javascript} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://jhorman-reto-frontend2.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={retoFrontendPicture3} alt="" />
+                        <img className='Projects__img' src={images.retoFrontend3} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={htmlIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
+                            <img className='Projects__icons' src={images.html} />
+                            <img className='Projects__icons' src={images.css} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href="https://portfolio-maqueta-jhorman.netlify.app/" target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={retoFrontendPicture4} alt="" />
+                        <img className='Projects__img' src={images.retoFrontend4} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={htmlIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
-                            <img className='Projects__icons' src={javascriptIcon} alt="" />
+                            <img className='Projects__icons' src={images.html} />
+                            <img className='Projects__icons' src={images.css} alt="" />
+                            <img className='Projects__icons' src={images.javascript} alt="" />
                         </div>
                     </div>
                 </a>
                 <a href='https://jhorman-apisrickandmorty.netlify.app/' target='_blank'>
                     <div className="Projects__card">
-                        <img className='Projects__img' src={rickAndMortyVanilaPicture} alt="" />
+                        <img className='Projects__img' src={images.rickAndMortyVanila} alt="" />
                         <h3>Herramientas usadas</h3>
                         <div className="Projects__icons--content">
-                            <img className='Projects__icons' src={htmlIcon} />
-                            <img className='Projects__icons' src={cssIcon} alt="" />
-                            <img className='Projects__icons' src={javascriptIcon} alt="" />
+                            <img className='Projects__icons' src={images.html} />
+                            <img className='Projects__icons' src={images.css} alt="" />
+                            <img className='Projects__icons' src={images.javascript} alt="" />
                         </div>
                     </div>
 
