e509ccdc77833cf1346d99b647a49db22c834c2a
jhorman9 <jhosmita9@gmail.com>
2022-12-16 22:28:07 -0500

    actualizar

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo/orangeLogo.png'
 import '../styles/Nav.css'
 
 const Navbar = () => {
+
+  const [closeNav, setCloseNav] = useState(true);
+
+  const handleShow = () => {
+    setCloseNav(!closeNav)
+  }
+
+  console.log
     return (
         <header>
           <nav>
             <div className='header--logo'>
               <img className='header--logo__img' style={{width: '100px', height:'75px'}} src={logo} alt="Jhorman Nieto P" />
             </div>
-              <ul className='header--link'>
-                <li><Link to={'/'}>Inicio</Link></li>
-                <li><Link to={'aboutme'}>Sobre mí</Link></li>
-                <li><Link to={'projects'}>Proyectos</Link></li>
-                <li><Link to={'contact'}>Contactame</Link></li>
+                <i onClick={handleShow} className="fa-solid fa-bars"></i>
+              <ul className={`${closeNav ? '' : 'showNavResponsive'}`}>
+                <li onClick={handleShow} className={`${closeNav ? 'hiddenNav' : ''}`}><Link to={'/'}>Inicio</Link></li>
+                <li onClick={handleShow} className={`${closeNav ? 'hiddenNav' : ''}`}><Link to={'aboutme'}>Sobre mí</Link></li>
+                <li onClick={handleShow} className={`${closeNav ? 'hiddenNav' : ''}`}><Link to={'projects'}>Proyectos</Link></li>
+                <li onClick={handleShow} className={`${closeNav ? 'hiddenNav' : ''}`}><Link to={'contact'}>Contactame</Link></li>
               </ul>
           </nav>
         </header>
--- src/assets/page/AboutMe.jsx
+++ src/assets/page/AboutMe.jsx
@@ -5,6 +5,11 @@ const AboutMe = () => {
     return (
         <div className='About-me'>
             <h2>Sobre mí</h2>
+            <p>Mi nombre es Jhorman Nieto, tengo 25 años de edad, nacionalidad venezolana. Actualmente he creado proyectos usando <span>HTML5, CSS3 y JAVASCRIPT</span> las bases del desarrollo web. También he elaborado otros proyectos con <span>REACT JS, REDUX, BOOSTRAP-5, BOOSTRAP-REACT Y REACT-ROUTE</span>. Soy estudiante de Licenciatura en administración técnologica y redes informaticas en la universidad Hossana ubicado en PANAMÁ - CIUDAD DE PANAMÁ.
+            <br/> 
+            He hecho varios cursos en una plataforma para estudiantes llamado UDEMY, me gusta aprender nuevas tecnologias en el cual sigo tomando cursos para seguir obteniendo más conocimientos, mi meta es ser FULL-STACK, por tanto he estado especializandome en el area de desarrollo web FULL-STACK.
+            <br/>
+            </p>
         </div>
     );
 };
